7daa4b999685cee3ee4b568aa3588712df4da478
billhamilton68 <billhamilton68@gmail.com>
2023-05-15 06:29:41 -0700

    update 2

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'root',
+      database: 'classlist_db'
+    },
+    console.log(`Connected to the classlist_db database.`)
+  );
+
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
